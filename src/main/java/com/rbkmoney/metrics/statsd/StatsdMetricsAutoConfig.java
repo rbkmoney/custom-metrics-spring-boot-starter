faee6cbc4be53516f101da0c0b9c2afefa15695b
Pavel Popov <tolkonepiu@users.noreply.github.com>
2018-11-28 18:18:38 +0300

    Add application tag (#2)
    * Add application tag
    
    * Add spring.application.name parameter in Readme

--- src/main/java/com/rbkmoney/metrics/statsd/StatsdMetricsAutoConfig.java
+++ src/main/java/com/rbkmoney/metrics/statsd/StatsdMetricsAutoConfig.java
@@ -1,11 +1,21 @@
 package com.rbkmoney.metrics.statsd;
 
+        import io.micrometer.core.instrument.MeterRegistry;
+        import io.micrometer.spring.autoconfigure.MeterRegistryCustomizer;
+        import org.springframework.beans.factory.annotation.Value;
         import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
         import org.springframework.context.annotation.Bean;
         import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StatsdMetricsAutoConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(@Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config()
+                .commonTags("application", applicationName);
+    }
 
     @Bean
     @ConditionalOnMissingBean
