e35c7c76787411405ffcd5bb532fe8ca7372d1ea
Pavel Popov <tolkonepiu@users.noreply.github.com>
2018-12-05 15:19:21 +0300

    Bump to Spring 2.1.0 (#3)

--- src/main/java/com/rbkmoney/metrics/statsd/StatsdMetricsAutoConfig.java
+++ src/main/java/com/rbkmoney/metrics/statsd/StatsdMetricsAutoConfig.java
@@ -1,8 +1,8 @@
 package com.rbkmoney.metrics.statsd;
 
         import io.micrometer.core.instrument.MeterRegistry;
-        import io.micrometer.spring.autoconfigure.MeterRegistryCustomizer;
         import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
         import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
         import org.springframework.context.annotation.Bean;
         import org.springframework.context.annotation.Configuration;
