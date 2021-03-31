3a2f6b2ee99801a94af4ad53386618834026c06e
Anatoly Karlov <karleowne@gmail.com>
2021-03-31 23:13:43 +0700

    migrate on library-parent-pom

--- src/main/java/com/rbkmoney/metrics/CustomMetricBinder.java
+++ src/main/java/com/rbkmoney/metrics/CustomMetricBinder.java
@@ -3,7 +3,6 @@ package com.rbkmoney.metrics;
 import com.sun.management.GarbageCollectorMXBean;
 import com.sun.management.OperatingSystemMXBean;
 import com.sun.management.UnixOperatingSystemMXBean;
-import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.Tags;
@@ -13,6 +12,8 @@ import java.lang.management.ClassLoadingMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.MemoryType;
+
+import static io.micrometer.core.instrument.Gauge.builder;
 
 public class CustomMetricBinder implements MeterBinder {
 
@@ -29,27 +30,33 @@ public class CustomMetricBinder implements MeterBinder {
         ManagementFactory.getGarbageCollectorMXBeans().stream()
                 .map(o -> (GarbageCollectorMXBean) o)
                 .forEach(garbageCollectorMXBean -> {
-                    Gauge.builder("jvm.gc.count", garbageCollectorMXBean, GarbageCollectorMXBean::getCollectionCount)
+                    builder("jvm.gc.count", garbageCollectorMXBean, GarbageCollectorMXBean::getCollectionCount)
                             .baseUnit("collections").register(meterRegistry);
-                    Gauge.builder("jvm.gc.time", garbageCollectorMXBean, GarbageCollectorMXBean::getCollectionTime)
+                    builder("jvm.gc.time", garbageCollectorMXBean, GarbageCollectorMXBean::getCollectionTime)
                             .baseUnit("millis").register(meterRegistry);
                 });
     }
 
     private void registerFileDescriptionMetrics(MeterRegistry meterRegistry) {
         ManagementFactory.getPlatformMXBeans(UnixOperatingSystemMXBean.class).forEach(classLoadingMXBean -> {
-            Gauge.builder("jvm.max.file.descriptor.count", classLoadingMXBean, UnixOperatingSystemMXBean::getMaxFileDescriptorCount)
+            builder(
+                    "jvm.max.file.descriptor.count",
+                    classLoadingMXBean,
+                    UnixOperatingSystemMXBean::getMaxFileDescriptorCount)
                     .baseUnit("descriptors").register(meterRegistry);
-            Gauge.builder("jvm.open.file.descriptor.count", classLoadingMXBean, UnixOperatingSystemMXBean::getOpenFileDescriptorCount)
+            builder(
+                    "jvm.open.file.descriptor.count",
+                    classLoadingMXBean,
+                    UnixOperatingSystemMXBean::getOpenFileDescriptorCount)
                     .baseUnit("descriptors").register(meterRegistry);
         });
     }
 
     private void registerClassesMetrics(MeterRegistry meterRegistry) {
         ManagementFactory.getPlatformMXBeans(ClassLoadingMXBean.class).forEach(classLoadingMXBean -> {
-            Gauge.builder("jvm.classes.total.loaded", classLoadingMXBean, ClassLoadingMXBean::getTotalLoadedClassCount)
+            builder("jvm.classes.total.loaded", classLoadingMXBean, ClassLoadingMXBean::getTotalLoadedClassCount)
                     .baseUnit("classes").register(meterRegistry);
-            Gauge.builder("jvm.classes.loaded", classLoadingMXBean, ClassLoadingMXBean::getLoadedClassCount)
+            builder("jvm.classes.loaded", classLoadingMXBean, ClassLoadingMXBean::getLoadedClassCount)
                     .baseUnit("classes").register(meterRegistry);
         });
     }
@@ -59,28 +66,34 @@ public class CustomMetricBinder implements MeterBinder {
             String area = memoryPoolBean.getType().equals(MemoryType.HEAP) ? "heap" : "nonheap";
             Iterable<Tag> tags = Tags.of("id", memoryPoolBean.getName(), "area", area);
 
-            Gauge.builder("jvm.memory.used", memoryPoolBean,
-                    (mem) -> (double) mem.getUsage().getUsed()).baseUnit("bytes").tags(tags).register(meterRegistry);
-            Gauge.builder("jvm.memory.committed", memoryPoolBean,
-                    (mem) -> (double) mem.getUsage().getCommitted()).baseUnit("bytes").tags(tags).register(meterRegistry);
-            Gauge.builder("jvm.memory.max", memoryPoolBean,
-                    (mem) -> (double) mem.getUsage().getMax()).baseUnit("bytes").tags(tags).register(meterRegistry);
+            builder("jvm.memory.used", memoryPoolBean, (mem) -> (double) mem.getUsage().getUsed())
+                    .baseUnit("bytes").tags(tags).register(meterRegistry);
+            builder("jvm.memory.committed", memoryPoolBean, (mem) -> (double) mem.getUsage().getCommitted())
+                    .baseUnit("bytes").tags(tags).register(meterRegistry);
+            builder("jvm.memory.max", memoryPoolBean, (mem) -> (double) mem.getUsage().getMax())
+                    .baseUnit("bytes").tags(tags).register(meterRegistry);
         });
     }
 
     private void registerJvmMetrics(MeterRegistry meterRegistry) {
         ManagementFactory.getPlatformMXBeans(OperatingSystemMXBean.class).forEach(memoryPoolBean -> {
-            Gauge.builder("free.physical.memory.size", memoryPoolBean, OperatingSystemMXBean::getFreePhysicalMemorySize)
+            builder(
+                    "free.physical.memory.size",
+                    memoryPoolBean,
+                    OperatingSystemMXBean::getFreePhysicalMemorySize)
                     .baseUnit("bytes").register(meterRegistry);
-            Gauge.builder("total.physical.memory.size", memoryPoolBean, OperatingSystemMXBean::getTotalPhysicalMemorySize)
+            builder(
+                    "total.physical.memory.size",
+                    memoryPoolBean,
+                    OperatingSystemMXBean::getTotalPhysicalMemorySize)
                     .baseUnit("bytes").register(meterRegistry);
-            Gauge.builder("system.cpu.load", memoryPoolBean, OperatingSystemMXBean::getSystemCpuLoad)
+            builder("system.cpu.load", memoryPoolBean, OperatingSystemMXBean::getSystemCpuLoad)
                     .baseUnit("bytes").register(meterRegistry);
-            Gauge.builder("process.cpu.load", memoryPoolBean, OperatingSystemMXBean::getProcessCpuLoad)
+            builder("process.cpu.load", memoryPoolBean, OperatingSystemMXBean::getProcessCpuLoad)
                     .baseUnit("bytes").register(meterRegistry);
-            Gauge.builder("total.swap", memoryPoolBean, OperatingSystemMXBean::getTotalSwapSpaceSize)
+            builder("total.swap", memoryPoolBean, OperatingSystemMXBean::getTotalSwapSpaceSize)
                     .baseUnit("bytes").register(meterRegistry);
-            Gauge.builder("free.swap", memoryPoolBean, OperatingSystemMXBean::getFreeSwapSpaceSize)
+            builder("free.swap", memoryPoolBean, OperatingSystemMXBean::getFreeSwapSpaceSize)
                     .baseUnit("bytes").register(meterRegistry);
         });
     }
--- src/main/java/com/rbkmoney/metrics/MetricsAutoConfig.java
+++ src/main/java/com/rbkmoney/metrics/MetricsAutoConfig.java
@@ -1,18 +1,19 @@
 package com.rbkmoney.metrics;
 
-        import io.micrometer.core.instrument.MeterRegistry;
-        import org.springframework.beans.factory.annotation.Value;
-        import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
-        import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.Configuration;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricsAutoConfig {
 
     @Bean
     @ConditionalOnMissingBean
-    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(@Value("${spring.application.name}") String applicationName) {
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(
+            @Value("${spring.application.name}") String applicationName) {
         return registry -> registry.config()
                 .commonTags("application", applicationName);
     }
